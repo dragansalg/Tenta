549866174fba0c23fa93aba1d2eab5338a98bd3f
dragansalg <dragansalg@gmail.com>
2020-10-01 11:59:29 +0200

    added testfile under testbranch

---
+++ 01.js
@@ -0,0 +1,12 @@
+/*
+
+Gör en for-loop som skriver ut 1-10, en rad i taget.
+Denna loop skriver ut 1-10, en rad i taget, såvida inte jag missuppfattat detta...
+*/
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of d6671a2... Removed solution
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
