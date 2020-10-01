68d7f0eb504fa0c0209395f637f32763b17d7033
dragansalg <70744244+dragansalg@users.noreply.github.com>
2020-10-01 13:33:56 +0200

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,12 +1,8 @@
 /*
 
 Gör en for-loop som skriver ut 1-10, en rad i taget.
-Denna loop skriver ut 1-10, en rad i taget, såvida inte jag missuppfattat detta...
 */
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of d6671a2... Removed solution
-for (let i = 1; i <= 10; i++) {
+for (let i = 15; i <= 25; i++) {
     console.log(i);
 }
